35208dcac562f90eed0951290f2b3d14577d7810
littlekath25 <katherine-fu@outlook.com>
2022-05-25 14:47:22 +0200

    stack functions

--- includes/stack.hpp
+++ includes/stack.hpp
@@ -7,21 +7,30 @@ class stack
 	private:
 	
 	public:
-		stack();
+		explicit 			stack (const container_type& ctnr = container_type());
 		~stack();
 
 		// empty
+		bool 				empty() const;
+
 		// size
+		size_type 			size() const;
+
 		// top
+		value_type& 		top();
+		const value_type& 	top() const;
+
 		// push
+		void 				push (const value_type& val);
+
 		// pop
+		void 				pop();
 
 		stack &operator=(const stack &copy);
-		// operator[]
-		// operator==
-		// operator!=
-		// operator<
-		// operator<=
-		// operator>
-		// operator>=
+        template <class T, class Alloc> bool operator== (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc> bool operator!= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc> bool operator<  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc> bool operator<= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc> bool operator>  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
+		template <class T, class Alloc> bool operator>= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 };
