8af1c8a7927ad82f2d33adf46c4268eba481d0cb
Katherine <katherine-fu@outlook.com>
2022-05-27 11:36:29 +0200

    commit

--- includes/map.hpp
+++ includes/map.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-
 template < class Key,                                     // map::key_type
            class T,                                       // map::mapped_type
            class Compare = less<Key>,                     // map::key_compare
